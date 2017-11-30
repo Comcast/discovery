fcb694ed692ffa38d93e4c48175b6358c9c387dd
Trent Schmidt <trent_schmidt@cable.comcast.com>
2017-11-29 16:27:30 -0800

    Only supply params if they contain strings

--- reagent/src/main/rpmroot/bin/reagent.sh
+++ reagent/src/main/rpmroot/bin/reagent.sh
@@ -9,24 +9,49 @@ CONF_DIR=$APP_HOME/conf
 RUN_DIR=/var/run/$NAME
 LOG_DIR=/var/log/$NAME
 
+build_params=""
+
 # source properties via facter if not defined
 if [ -z "$zookeeper_connection" ]; then
     zookeeper_connection=`facter zookeeper_connection`
 fi
+if [ -n "$zookeeper_connection" ]; then
+    build_params="$build_params -z $zookeeper_connection"
+fi
+
 if [ -z "$ipaddress" ]; then
     ipaddress=`facter ipaddress`
 fi
+if [ -n "$ipaddress" ]; then
+    build_params="$build_params -i $ipaddress"
+fi
+
 if [ -z "$service_spec" ]; then
     service_spec=`facter service_spec`
 fi
+if [ -n "$service_spec" ]; then
+    build_params="$build_params -s $service_spec"
+fi
+
 if [ -z "$flavor" ]; then
     flavor=`facter flavor`
 fi
+if [ -n "$flavor" ]; then
+    build_params="$build_params -f $flavor"
+fi
+
 if [ -z "$region" ]; then
     region=`facter region`
 fi
+if [ -n "$region" ]; then
+    build_params="$build_params -r $region"
+fi
+
 if [ -z "$availability_zone" ]; then
     availability_zone=`facter availability_zone`
+fi
+if [ -n "$availability_zone" ]; then
+    build_params="$build_params -a $availability_zone"
 fi
 
 # If JAVA_OPTS are set, then it will completely override defaults.
@@ -38,7 +63,7 @@ if [ "$1" != "--nodaemon" ]; then
     nohup java $JAVA_OPTS \
         -cp $CONF_DIR:$APP_HOME/lib/* \
         com.comcast.tvx.cloud.RegistrationMain \
-        -z $zookeeper_connection -i $ipaddress -s $service_spec -f $flavor -r $region -a $availability_zone \
+        $build_params \
         > $LOG_DIR/$NAME.out 2>&1 &
 
     echo $! > $RUN_DIR/$NAME.pid
@@ -47,6 +72,6 @@ else
     java $JAVA_OPTS \
         -cp $CONF_DIR:$APP_HOME/lib/* \
         com.comcast.tvx.cloud.RegistrationMain \
-        -z $zookeeper_connection -i $ipaddress -s $service_spec -f $flavor -r $region -a $availability_zone
+        $build_params
 
 fi
